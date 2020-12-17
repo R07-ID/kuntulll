a2d9d72443f085a48ad4c78718150ba084ad7088
R07-ID <76058962+R07-ID@users.noreply.github.com>
2020-12-18 00:56:58 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,20 @@
+termux-setup-storage
+pkg upgrade
+pkg update
+pkg install git
+pkg install tesseract
+pkg install unzip
+git clone https://github.com/R07-ID/kuntulll
+ls 
+cd kuntulll
+unzip ROTBOT
+pkg install ffmpeg 
+pkg install wget 
+pkg install nodejs
+npm i -g cwebp
+npm i node-tesseract-ocr
+npm i -g ytdl
+npm i
+npm i got
+node index js
+Enjoiiii scann qr nya!!!!!!!!!!!
